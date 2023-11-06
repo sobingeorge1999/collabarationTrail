6546084710be485f5a49c08e67268043b5bb141f
amal-1898 <amalam1898@gmail.com>
2023-11-06 22:12:57 +0530

    Mechanic list

--- src/Components/mechanic/Viewmech.js
+++ src/Components/mechanic/Viewmech.js
@@ -1,31 +1,105 @@
-import React from 'react'
+import axios from 'axios';
+import useParams from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
 
 function Viewmech() {
+
+  const [data, setData] = useState([])
+
+  const id = localStorage.getItem("ownerid")
+  useEffect(() => {
+    console.log(localStorage.getItem("ownerid"));
+
+    axios.post(`http://localhost:4000/workshop/viewmech/${id}`)
+      .then((a) => {
+        console.log(a.data.result);
+        // setData(a.data.result)
+      },[])
+
+  })
+
+
+  // const remove =(id) => {
+  //   axios.delete(`http://localhost:4000/workshop/removemech/${id}`).then((data) => {
+  //     console.log(data);
+  //     if (data.data.status == 200) {
+  //       alert("Mechanic removed")
+  //       window.location.reload()
+  //     }
+  //   })
+  // }
+
   return (
     <div>
-          <div class="mb-4 header">
-              <h2 style={{ textAlign: 'center' }}>View Mechanics</h2>
-          </div>
-          <div class="tblbox">
-              <table class="table table-hover">
-                <th>No.</th>
-                <th>Name</th>
-                <th>Category</th>
-                <th>Availability</th>
-                <th>Edit/Delete</th>
+      <div>
+        <div>
+          <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="/workshop">Workshop</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a
+                      class="nav-link"
+                      aria-current="page"
+                      href="/workshop/Ownerdashboard">Dashboard</a>
+                  </li>
+                  <li class="nav-item">
+                    <a
+                      class="nav-link active"
+                      href="/workshop/Viewmech"
+                    >View Mechanics</a>
+                  </li>
+                  <li class="nav-item">
+                    <a
+                      class="nav-link"
+                      href="/workshop/AddMech"
+                    >Add Mechanic</a>
+                  </li>
+                  <li class="nav-item">
+                    <a
+                      class="nav-link"
+                      href="/workshop/Viewshop"
+                    >My Shop</a>
+                  </li>
+                </ul>
+                <span class="navbar-text">
+                  <a class="nav-link" href="/workshop/WorkshopLogin">Logout</a>
+                </span>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+      <div class="mb-4 header">
+        <h2 style={{ textAlign: 'center' }}>View Mechanics</h2>
+      </div>
+      <div class="tblbox">
+        <table class="table table-hover">
+          <th>Name</th>
+          <th>Category</th>
+          <th>Availability</th>
+          <th>Edit/Delete</th>
+          {/* {data.map((t) => {
+            return (
                 <tr>
-                    <td>1.</td>
-                    <td>Amal</td>
-                    <td>Mechanical work</td>
+                    <td>{t.name}</td>
+                    <td>{t.category}</td>
                     <td>Not available</td>
                     <td>
-                        <button>Edit</button>
-                        <button>Delete</button>
+                        <button 
+                    // onClick={() => { remove(t._id) }}
+                        >Delete</button>
                     </td>
                 </tr>
-              </table>
-          </div>
-          <div><button>+Add</button></div>
+            )
+          })} */}
+        </table>
+      </div>
+      <div><button ><a href="/workshop/AddMech">+ Add</a></button></div>
     </div>
   )
 }
